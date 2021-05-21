b136b075d56710e9cf51c805ef1bd98e645df88a
leekichang <1kichang@yonsei.ac.kr>
2021-05-21 09:12:10 +0900

    210521 09:12

--- Clock_example/clock.py
+++ Clock_example/clock.py
@@ -10,14 +10,12 @@ from kivy.utils import get_color_from_hex
 Window.size = (400, 300)
 Window.clearcolor = get_color_from_hex('#3385ff')
  
- 
 class ClockApp(App):
     def update_clock(self, nap):
         self.root.ids.time.text = strftime('   Clock\n[b]%H[/b]:%M:%S')
  
     def on_start(self):
         Clock.schedule_interval(self.update_clock, 1)
- 
  
 if __name__ == '__main__':
     ClockApp().run()
